8bee249cb99658d7f2df4bf5868fb61d2b5ae5e1
akhmad.kha <akhmadklf1@gmail.com>
2020-11-05 10:22:40 +0700

    git

---
+++ precache-manifest.dbc70f2aebf74b6f0f47fbebce4f3794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf27a2f0e2e4dfd3bbff5ef4ef658d89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f1eb344c97196d0dcf",
+    "url": "/static/css/main.891ebe8b.chunk.css"
+  },
+  {
+    "revision": "51a0d01532ce99f62ad7",
+    "url": "/static/js/2.b4f2fbd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4f2fbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f1eb344c97196d0dcf",
+    "url": "/static/js/main.3471b7f9.chunk.js"
+  },
+  {
+    "revision": "2c948fb955bf7a4e3e08",
+    "url": "/static/js/runtime-main.48a2c9f5.js"
+  },
+  {
+    "revision": "d31ba4950980e352ae71f7d8981047e3",
+    "url": "/static/media/logo.d31ba495.svg"
+  }
+]);
